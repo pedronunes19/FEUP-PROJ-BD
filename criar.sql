a1ff791ef213ff36295d65bc756391edb468e495
pedronunes19 <pedromiguelmnunes@gmail.com>
2022-01-23 16:59:20 +0000

    add files up to 2nd delivery

---
+++ criar.sql
@@ -0,0 +1,153 @@
+drop table if exists Linha;
+create table Linha(
+    idLinha int primary key,
+    Número int not null unique,
+    EmFuncionamento int not null,
+    LimiteDaLnha1 int references Paragem not null,
+    LimiteDaLnha2 int references Paragem not null,
+    constraint bool check(EmFuncionamento=0 or EmFuncionamento=1)
+);
+
+
+drop table if exists Paragem;
+create table Paragem(
+    idParagem int primary key,
+    Nome varchar(30) not null unique,
+    EmFuncionamento int not null,
+    idZona int references Zona not null,
+    constraint bool check(EmFuncionamento=0 or EmFuncionamento=1)
+);
+
+
+drop table if exists Zona;
+create table Zona(
+    idZona int primary key,
+    Nome char(4) not null unique
+);
+
+
+drop table if exists Horário;
+create table Horário(
+    idHorário int primary key,
+    idLinha int references Linha not null,
+    idTipoHorário int references TipoHorário not null
+);
+
+
+drop table if exists TipoHorário;
+create table TipoHorário(
+    idTipoHorário int primary key,
+    Nome varchar(20) not null unique,
+    constraint TipoHorárioVálido check(Nome='Dias Úteis' or Nome='Sábados' or Nome='Domingos/Feriados' or Nome='Verão' or Nome='Férias Escolares')
+);
+
+
+drop table if exists AlteraçãoServiço;
+create table AlteraçãoServiço(
+    idAlteraçãoServiço int primary key,
+    Motivo varchar(100),
+    DataInício date not null,
+    DataFinal date not null,
+    idTipoAlteração int references TipoAlteraçãoServiço not null,
+    constraint PeríodoTempoVálido check(DataFinal >= DataInício)
+);
+
+
+drop table if exists TipoAlteraçãoServiço;
+create table TipoAlteraçãoServiço(
+    idTipoAlteraçãoServiço int primary key,
+    Nome varchar(20) not null unique,
+    constraint TipoAlteraçãoVálido check(Nome='Linha' or Nome='Paragem' or Nome='Horário')
+);
+
+
+drop table if exists Passagem;
+create table Passagem(
+    idPassagem int primary key,
+    idHorário int references Horário not null,
+    idLinha int references Linha not null,
+    idParagem int references Paragem not null,
+    Hora time not null  -- time doesn't really work as desired but I suppose we can use constraints
+);
+
+
+drop table if exists Autocarro;
+create table Autocarro(
+    idAutocarro int primary key,
+    Matricula char(8) not null unique,
+    EmFuncionamento int not null,
+    idTipoAutocarro int references TipoAutocarro not null,
+    constraint bool check(EmFuncionamento=0 or EmFuncionamento=1)
+);
+
+
+drop table if exists TipoAutocarro;
+create table TipoAutocarro(
+    idTipoAutocarro int primary key,
+    Nome varchar(20) not null unique,
+    MaxLugaresSentados int not null,
+    MaxLugaresPé int not null,
+    constraint LugaresPositivos check(MaxLugaresPé>0 and MaxLugaresSentados>0),
+    constraint TipoAutocarroVálido check(Nome='Standard' or Nome='Articulado' or Nome = 'Dois Pisos' or Nome='Mini')
+);
+
+
+drop table if exists Condutor;
+create table Condutor(
+    idCondutor int primary key,
+    Nome varchar(80) not null,
+    Contacto char(9) unique not null,
+    DataNascimento date not null,
+    Morada varchar(200) not null,
+    Nacionalidade varchar(20) not null,
+    NumDocIdentificacao varchar(12) not null unique
+);
+
+
+
+--associações
+
+drop table if exists LinhaDoAutocarro;
+create table LinhaDoAutocarro(
+    idAutocarro int references Autocarro not null,
+    idLinha int references Linha not null,
+    primary key (idAutocarro, idLinha)
+);
+
+
+drop table if exists CondutorDoAutocarro;
+create table CondutorDoAutocarro(
+    idAutocarro int references Autocarro not null,
+    idCondutor int references Condutor not null,
+    primary key (idAutocarro, idCondutor)
+);
+
+
+drop table if exists AlteraçãoHorário;
+create table AlteraçãoHorário(
+    idALteraçãoServiço int references AlteraçãoServiço primary key not null,
+    idPassagem int references Passagem not null
+);
+
+
+drop table if exists DesativaçãoLinha;
+create table DesativaçãoLinha(
+    idALteraçãoServiço int references AlteraçãoServiço primary key not null,
+    idLinha int references Linha not null
+);
+
+
+drop table if exists DesativaçãoParagem;
+create table DesativaçãoParagem(
+    idALteraçãoServiço int references AlteraçãoServiço primary key not null,
+    idParagem int references Paragem not null
+);
+
+
+
+
+
+
+
+
+
---
+++ povoar.sql
@@ -0,0 +1,279 @@
+PRAGMA foreign_keys = ON;
+
+--Zona
+insert into Zona values(1, 'PRT1');
+insert into Zona values(2, 'PRT2');
+insert into Zona values(3, 'PRT3');
+insert into Zona values(4, 'MAI1');
+insert into Zona values(5, 'MAI2');
+insert into Zona values(6, 'MAI3');
+insert into Zona values(7, 'MAI4');
+insert into Zona values(8, 'MAI5');
+insert into Zona values(9, 'MST1');
+insert into Zona values(10, 'MST2');
+insert into Zona values(11, 'GDM1');
+insert into Zona values(12, 'GDM2');
+insert into Zona values(13, 'GDM3');
+insert into Zona values(14, 'GDM4');
+insert into Zona values(15, 'VNG1');
+insert into Zona values(16, 'VNG2');
+insert into Zona values(17, 'VNG3');
+insert into Zona values(18, 'VNG4');
+insert into Zona values(19, 'VNG5');
+insert into Zona values(20, 'VNG6');
+
+--Paragem
+insert into Paragem values(1, 'PASSEIO ALEGRE', 1, 2);
+insert into Paragem values(2, 'PRAIA DO OURIGO', 1, 2);
+insert into Paragem values(3, 'PRAIA DOS INGLESES', 1, 2);
+insert into Paragem values(4, 'CRASTO', 1, 2);
+insert into Paragem values(5, 'JACINTO NUNES', 1, 2);
+insert into Paragem values(6, 'MOLHE', 1, 2);
+insert into Paragem values(7, 'NEVOGILDE', 1, 2);
+insert into Paragem values(8, 'LGO. NEVOGILDE', 1, 2);
+insert into Paragem values(9, 'R. CRASTO', 1, 2);
+insert into Paragem values(10, 'LICEU GARCIA ORTA', 1, 2);
+insert into Paragem values(11, 'AV. DA BOAVISTA', 1, 2);
+insert into Paragem values(12, 'FONTE DA MOURA', 1, 2);
+insert into Paragem values(13, 'BAIRRO DA VILARINHA', 1, 2);
+insert into Paragem values(14, 'BAIRRO DA PREVIDÊNCIA', 1, 2);
+insert into Paragem values(15, 'ALBERTO MACEDO', 1, 2);
+insert into Paragem values(16, 'S. JOÃO DE BRITO', 1, 2);
+insert into Paragem values(17, 'S. JOÃO BOSCO', 1, 2);
+insert into Paragem values(18, 'AV. DO BESSA', 1, 1);
+insert into Paragem values(19, 'SIDÓNIO PAIS', 1, 1);
+insert into Paragem values(20, 'CASA DE SAÚDE DA BOAVISTA', 1, 1);
+insert into Paragem values(21, 'GRACIOSA', 1, 1);
+insert into Paragem values(22, 'BOAVISTA-CASA DA MÚSICA', 1, 1);
+insert into Paragem values(23, 'BOAVISTA BRASÍLIA', 1, 1);
+insert into Paragem values(24, 'HOSPITAL MILITAR', 1, 1);
+insert into Paragem values(25, 'CARVALHOSA', 1, 1);
+insert into Paragem values(26, 'FIGUEIROA', 1, 1);
+insert into Paragem values(27, 'PR. DA REPÚBLICA', 1, 1);
+insert into Paragem values(28, 'GONÇALO CRISTOVÃO', 1, 1);
+insert into Paragem values(29, 'TRINDADE', 1, 1);
+insert into Paragem values(30, 'AV. ALIADOS', 1, 1);
+
+--Linha
+insert into Linha values(1, 202, 1, 1, 30);
+
+--Condutor
+insert into Condutor values(1, 'Tiago Araújo', '919874321', '1993-12-06', 'Rua de Passos Manuel 137, Porto', 'Portugues', '033430166');
+insert into Condutor values(2, 'Ana Guedes', '911345987', '1995-06-03', 'Rua dos Mártires da Liberdade 134, Porto', 'Portugues', '166430033');
+insert into Condutor values(3, 'Bernardo Silva', '923546798', '1997-03-01', 'Rua da Madeira 84, Porto', 'Portugues', '642834961');
+
+
+--TipoAutocarro
+insert into TipoAutocarro values(1, 'Standard', 25, 45);
+insert into TipoAutocarro values(2, 'Articulado', 45, 85);
+insert into TipoAutocarro values(3, "Dois Pisos", 50, 110);
+insert into TipoAutocarro values(4, "Mini", 18, 25);
+
+
+--Autocarro
+insert into Autocarro values(1, 'AB-09-CD', 1, 1);
+insert into Autocarro values(2, 'EF-18-GH', 1, 2);
+insert into Autocarro values(3, 'ZX-55-CV', 0, 4);
+
+
+--CondutorDoAutocarro
+insert into CondutorDoAutocarro values(1, 1);
+insert into CondutorDoAutocarro values(1, 2);
+insert into CondutorDoAutocarro values(2, 1);
+insert into CondutorDoAutocarro values(2, 3);
+insert into CondutorDoAutocarro values(3, 3);
+
+--LinhaDoAutocarro
+insert into LinhaDoAutocarro values(1, 1);
+insert into LinhaDoAutocarro values(2, 1);
+insert into LinhaDoAutocarro values(3, 1);
+
+
+--TipoHorário
+insert into TipoHorário values(1, 'Dias Úteis');
+insert into TipoHorário values(2, 'Sábados');
+insert into TipoHorário values(3, 'Domingos/Feriados');
+insert into TipoHorário values(4, 'Verão');
+insert into TipoHorário values(5, 'Férias Escolares');
+
+
+--Horário
+insert into Horário values(1, 1, 1);
+insert into Horário values(2, 1, 2);
+
+
+--Passagem
+insert into Passagem values(1, 1, 1, 1, '06:00');
+insert into Passagem values(2, 1, 1, 1, '06:30');
+insert into Passagem values(3, 1, 1, 1, '06:44');
+insert into Passagem values(4, 1, 1, 1, '06:57');
+insert into Passagem values(5, 1, 1, 1, '07:11');
+insert into Passagem values(6, 1, 1, 1, '07:36');
+insert into Passagem values(7, 1, 1, 1, '07:51');
+insert into Passagem values(8, 1, 1, 1, '08:08');
+insert into Passagem values(9, 1, 1, 1, '08:27');
+insert into Passagem values(10, 1, 1, 1, '08:45');
+insert into Passagem values(11, 1, 1, 1, '09:14');
+insert into Passagem values(12, 1, 1, 1, '09:30');
+insert into Passagem values(13, 1, 1, 1, '09:47');
+insert into Passagem values(14, 1, 1, 1, '10:06');
+insert into Passagem values(15, 1, 1, 1, '10:30');
+insert into Passagem values(16, 1, 1, 1, '10:54');
+insert into Passagem values(17, 1, 1, 1, '11:18');
+insert into Passagem values(18, 1, 1, 1, '11:42');
+insert into Passagem values(19, 1, 1, 1, '12:06');
+insert into Passagem values(20, 1, 1, 1, '12:30');
+insert into Passagem values(21, 1, 1, 1, '12:44');
+insert into Passagem values(22, 1, 1, 1, '12:53');
+insert into Passagem values(23, 1, 1, 1, '13:17');
+insert into Passagem values(24, 1, 1, 1, '13:41');
+insert into Passagem values(25, 1, 1, 1, '14:05');
+insert into Passagem values(26, 1, 1, 1, '14:26');
+insert into Passagem values(27, 1, 1, 1, '14:48');
+insert into Passagem values(28, 1, 1, 1, '15:08');
+insert into Passagem values(29, 1, 1, 1, '15:28');
+insert into Passagem values(30, 1, 1, 1, '15:48');
+insert into Passagem values(31, 1, 1, 1, '16:08');
+insert into Passagem values(32, 1, 1, 1, '16:19');
+insert into Passagem values(33, 1, 1, 1, '16:30');
+insert into Passagem values(34, 1, 1, 1, '16:51');
+insert into Passagem values(35, 1, 1, 1, '17:11');
+insert into Passagem values(36, 1, 1, 1, '17:35');
+insert into Passagem values(37, 1, 1, 1, '17:57');
+insert into Passagem values(38, 1, 1, 1, '18:17');
+insert into Passagem values(39, 1, 1, 1, '18:37');
+insert into Passagem values(40, 1, 1, 1, '18:59');
+insert into Passagem values(41, 1, 1, 1, '19:18');
+insert into Passagem values(42, 1, 1, 1, '19:31');
+insert into Passagem values(43, 1, 1, 1, '19:47');
+insert into Passagem values(44, 1, 1, 1, '20:06');
+insert into Passagem values(45, 1, 1, 1, '20:24');
+insert into Passagem values(46, 1, 1, 1, '20:40');
+insert into Passagem values(47, 1, 1, 1, '21:03');
+insert into Passagem values(48, 1, 1, 1, '21:25');
+
+insert into Passagem values(50, 1, 1, 30, '06:15');
+insert into Passagem values(51, 1, 1, 30, '06:40');
+insert into Passagem values(52, 1, 1, 30, '07:00');
+insert into Passagem values(53, 1, 1, 30, '07:15');
+insert into Passagem values(54, 1, 1, 30, '07:30');
+insert into Passagem values(55, 1, 1, 30, '07:45');
+insert into Passagem values(56, 1, 1, 30, '08:00');
+insert into Passagem values(57, 1, 1, 30, '08:15');
+insert into Passagem values(58, 1, 1, 30, '08:30');
+insert into Passagem values(59, 1, 1, 30, '08:48');
+insert into Passagem values(60, 1, 1, 30, '09:06');
+insert into Passagem values(61, 1, 1, 30, '09:25');
+insert into Passagem values(62, 1, 1, 30, '09:38');
+insert into Passagem values(63, 1, 1, 30, '10:12');
+insert into Passagem values(64, 1, 1, 30, '10:36');
+insert into Passagem values(65, 1, 1, 30, '11:00');
+insert into Passagem values(66, 1, 1, 30, '11:24');
+insert into Passagem values(67, 1, 1, 30, '11:48');
+insert into Passagem values(68, 1, 1, 30, '12:12');
+insert into Passagem values(69, 1, 1, 30, '12:36');
+insert into Passagem values(70, 1, 1, 30, '13:00');
+insert into Passagem values(71, 1, 1, 30, '13:24');
+insert into Passagem values(72, 1, 1, 30, '13:45');
+insert into Passagem values(73, 1, 1, 30, '14:05');
+insert into Passagem values(74, 1, 1, 30, '14:25');
+insert into Passagem values(75, 1, 1, 30, '14:45');
+insert into Passagem values(76, 1, 1, 30, '15:05');
+insert into Passagem values(77, 1, 1, 30, '15:25');
+insert into Passagem values(78, 1, 1, 30, '15:35');
+insert into Passagem values(79, 1, 1, 30, '16:05');
+insert into Passagem values(80, 1, 1, 30, '16:25');
+insert into Passagem values(81, 1, 1, 30, '16:45');
+insert into Passagem values(82, 1, 1, 30, '17:05');
+insert into Passagem values(83, 1, 1, 30, '17:25');
+insert into Passagem values(84, 1, 1, 30, '17:45');
+insert into Passagem values(85, 1, 1, 30, '18:05');
+insert into Passagem values(86, 1, 1, 30, '18:25');
+insert into Passagem values(87, 1, 1, 30, '18:45');
+insert into Passagem values(88, 1, 1, 30, '19:05');
+insert into Passagem values(89, 1, 1, 30, '19:25');
+insert into Passagem values(90, 1, 1, 30, '19:45');
+insert into Passagem values(91, 1, 1, 30, '20:05');
+insert into Passagem values(92, 1, 1, 30, '20:30');
+insert into Passagem values(93, 1, 1, 30, '20:55');
+insert into Passagem values(94, 1, 1, 30, '21:25');
+
+insert into Passagem values(100, 2, 1, 1, '06:14');
+insert into Passagem values(101, 2, 1, 1, '06:44');
+insert into Passagem values(102, 2, 1, 1, '07:25');
+insert into Passagem values(103, 2, 1, 1, '08:06');
+insert into Passagem values(104, 2, 1, 1, '08:48');
+insert into Passagem values(105, 2, 1, 1, '09:30');
+insert into Passagem values(106, 2, 1, 1, '10:10');
+insert into Passagem values(107, 2, 1, 1, '10:50');
+insert into Passagem values(108, 2, 1, 1, '10:30');
+insert into Passagem values(109, 2, 1, 1, '11:30');
+insert into Passagem values(110, 2, 1, 1, '12:10');
+insert into Passagem values(111, 2, 1, 1, '12:50');
+insert into Passagem values(112, 2, 1, 1, '13:17');
+insert into Passagem values(113, 2, 1, 1, '13:32');
+insert into Passagem values(114, 2, 1, 1, '14:13');
+insert into Passagem values(115, 2, 1, 1, '14:48');
+insert into Passagem values(116, 2, 1, 1, '15:20');
+insert into Passagem values(117, 2, 1, 1, '15:50');
+insert into Passagem values(118, 2, 1, 1, '16:20');
+insert into Passagem values(119, 2, 1, 1, '16:50');
+insert into Passagem values(120, 2, 1, 1, '17:20');
+insert into Passagem values(121, 2, 1, 1, '17:50');
+insert into Passagem values(122, 2, 1, 1, '18:20');
+insert into Passagem values(123, 2, 1, 1, '18:48');
+insert into Passagem values(124, 2, 1, 1, '19:20');
+insert into Passagem values(125, 2, 1, 1, '20:00');
+insert into Passagem values(126, 2, 1, 1, '20:40');
+insert into Passagem values(127, 2, 1, 1, '21:23');
+
+insert into Passagem values(130, 2, 1, 30, '06:15');
+insert into Passagem values(131, 2, 1, 30, '06:55');
+insert into Passagem values(132, 2, 1, 30, '07:35');
+insert into Passagem values(133, 2, 1, 30, '08:15');
+insert into Passagem values(134, 2, 1, 30, '08:55');
+insert into Passagem values(135, 2, 1, 30, '09:35');
+insert into Passagem values(136, 2, 1, 30, '10:15');
+insert into Passagem values(137, 2, 1, 30, '10:55');
+insert into Passagem values(138, 2, 1, 30, '11:35');
+insert into Passagem values(139, 2, 1, 30, '12:15');
+insert into Passagem values(140, 2, 1, 30, '12:55');
+insert into Passagem values(141, 2, 1, 30, '13:35');
+insert into Passagem values(142, 2, 1, 30, '14:10');
+insert into Passagem values(143, 2, 1, 30, '14:40');
+insert into Passagem values(144, 2, 1, 30, '15:10');
+insert into Passagem values(145, 2, 1, 30, '15:40');
+insert into Passagem values(146, 2, 1, 30, '16:10');
+insert into Passagem values(147, 2, 1, 30, '16:40');
+insert into Passagem values(148, 2, 1, 30, '17:10');
+insert into Passagem values(149, 2, 1, 30, '17:40');
+insert into Passagem values(150, 2, 1, 30, '18:10');
+insert into Passagem values(151, 2, 1, 30, '18:45');
+insert into Passagem values(152, 2, 1, 30, '19:25');
+insert into Passagem values(153, 2, 1, 30, '20:05');
+insert into Passagem values(154, 2, 1, 30, '20:50');
+insert into Passagem values(155, 2, 1, 30, '21:20');
+
+
+--TipoAlteraçãoServiço
+insert into TipoAlteraçãoServiço values(1, 'Paragem');
+insert into TipoAlteraçãoServiço values(2, 'Linha');
+insert into TipoAlteraçãoServiço values(3, 'Horário');
+
+
+--AlteraçãoServiço
+insert into AlteraçãoServiço values(1, 'Desativação Paragem Passeio Alegre Obras', '2021-12-13', '2021-12-14', 1);
+insert into AlteraçãoServiço values(2, 'Desativação Linha 202', '2021-12-15', '2021-12-16', 2);
+insert into AlteraçãoServiço values(3, 'Falha Autocarro 202 Passeio Alegre 06:00', '2021-12-17', '2021-12-18', 3);
+insert into AlteraçãoServiço values(4, 'Falha Autocarro 202 Aliados 06:15 Sábado', '2021-12-18', '2021-12-19', 3);
+
+
+--AlteraçãoHorário
+insert into AlteraçãoHorário values(3, 1);
+insert into AlteraçãoHorário values(4, 130);
+
+--DesativaçãoLinha
+insert into DesativaçãoLinha values(2, 1);
+
+--DesativaçãoParagem
+insert into DesativaçãoParagem values(1, 1);

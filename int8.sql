d9c676ad1add0136066a78cb35578e495a0b4a6b
Miguel Montes <miguellpmm@gmail.com>
2022-01-26 17:33:33 +0000

    added final files

--- int8.sql
+++
@@ -1,7 +0,0 @@
---linhas afetadas por uma alteração de serviço do tipo 'DesativaçãoLinha'
-.mode columns
-.headers on
-.nullvalue NULL
-
-SELECT Número FROM Linha, DesativaçãoLinha
-WHERE Linha.idLinha = DesativaçãoLinha.idLinha;
